1e3ccccc8948b49b2d7a8aa3b9a305c0b09141bd
monty <velimir.vesselinov@gmail.com>
2017-03-17 09:59:44 -0600

    minor

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using Base.Test
+import Base.Test
 import ReusableFunctions
 
 ReusableFunctions.resetrestarts()
@@ -16,62 +16,62 @@ end
 
 for fp in [ReusableFunctions.maker3function(freuse, restartdir), ReusableFunctions.maker3function(freuse)]
 	for i = 1:2
-		@test fp(1) == 1
+		@Base.Test.test fp(1) == 1
 	end
 	#check to make sure it works if the jld file is corrupted
     ReusableFunctions.checkhashfilename(restartdir, 1)
 	hashfilename = ReusableFunctions.gethashfilename(restartdir, 1)
 	run(`bash -c "echo blah >$hashfilename"`)
 	for i = 1:2
-		@test fp(1) == 1
+		@Base.Test.test fp(1) == 1
 	end
 
 	t = @elapsed for i = 1:10
-		@test fp(i) == i
+		@Base.Test.test fp(i) == i
 	end
-	@test t > 0.5
-	@test t < 2.
+	@Base.Test.test t > 0.5
+	@Base.Test.test t < 2.
 
 	t = @elapsed for i = 1:10
-		@test fp(i) == i
+		@Base.Test.test fp(i) == i
 	end
-	@test t < 0.1
+	@Base.Test.test t < 0.1
 
 	d = Dict(zip([1, 2], [3, 4]))
 	for i = 1:2
-		@test fp(d) == d
+		@Base.Test.test fp(d) == d
 	end
 
 	t = @elapsed for i = 1:10
 		d = Dict(zip([1, 2], [i, i + 1]))
-		@test fp(d) == d
+		@Base.Test.test fp(d) == d
 	end
-	@test t > 0.5
-	@test t < 2.
+	@Base.Test.test t > 0.5
+	@Base.Test.test t < 2.
 
 	t = @elapsed for i = 1:10
 		d = Dict(zip([1, 2], [i, i + 1]))
-		@test fp(d) == d
+		@Base.Test.test fp(d) == d
 	end
-	@test t < 0.1
+	@Base.Test.test t < 0.1
 
 	v = zeros(10)
 	for i = 1:2
-		@test fp(v) == v
+		@Base.Test.test fp(v) == v
 	end
 
 	t = @elapsed for i = 1:10
 		v = i * ones(10)
-		@test fp(v) == v
+		@Base.Test.test fp(v) == v
 	end
-	@test t > 0.5
-	@test t < 2.
+	@Base.Test.test t > 0.5
+	@Base.Test.test t < 2.
 
 	t = @elapsed for i = 1:10
 		v = i * ones(10)
-		@test fp(v) == v
+		@Base.Test.test fp(v) == v
 	end
-	@test t < 0.1
+	@Base.Test.test t < 0.1
 end
 
 if isdir(restartdir)
@@ -87,31 +87,33 @@ r3g = ReusableFunctions.maker3function(greuse, restartdir, ["a", "b"], ["asdf", 
 d = Dict("a"=>1, "b"=>3)
 r = Dict("asdf"=>-2, "hjkl"=>3)
 for i = 1:2
-	@test r3g(d) == r
+	@Base.Test.test r3g(d) == r
 end
 
 #test to make sure it works if the JLD file is corrupted
 hashfilename = ReusableFunctions.gethashfilename(restartdir, d)
 run(`bash -c "echo blah >$hashfilename"`)
 for i = 1:2
-	@test r3g(d) == r
+	@Base.Test.test r3g(d) == r
 end
 
 t = @elapsed for i = 1:10
 	d = Dict(zip(["a", "b"], [i, i + 2]))
 	r = Dict("asdf"=>-2, "hjkl"=>i * (i + 2))
-	@test r3g(d) == r
+	@Base.Test.test r3g(d) == r
 end
-@test t > 0.5
-@test t < 2.
+@Base.Test.test t > 0.5
+@Base.Test.test t < 2.
 
 t = @elapsed for i = 1:10
 	d = Dict(zip(["a", "b"], [i, i + 2]))
 	r = Dict("asdf"=>-2, "hjkl"=>i * (i + 2))
-	@test r3g(d) == r
+	@Base.Test.test r3g(d) == r
 end
-@test t < 0.1
+@Base.Test.test t < 0.1
 
 if isdir(restartdir)
 	rm(restartdir, recursive=true)
 end
+
+:passed
