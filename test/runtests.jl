34b86f354a3f0175c54d7c823ab1f04c4177969a
monty <velimir.vesselinov@gmail.com>
2018-11-11 06:26:44 -0700

    global

--- test/runtests.jl
+++ test/runtests.jl
@@ -57,49 +57,49 @@ end
 			@Test.test fp(1) == 1
 		end
 
-		t = @elapsed for i = 1:10
+		global t = @elapsed for i = 1:10
 			@Test.test fp(i) == i
 		end
 		@Test.test t > 0.5
 		@Test.test t < 2.
 
-		t = @elapsed for i = 1:10
+		global t = @elapsed for i = 1:10
 			@Test.test fp(i) == i
 		end
 		@Test.test t < 4. # this is slow under 1.0
 
-		d = Dict(zip([1, 2], [3, 4]))
+		global d = Dict(zip([1, 2], [3, 4]))
 		for i = 1:2
 			@Test.test fp(d) == d
 		end
 
-		t = @elapsed for i = 1:10
+		global t = @elapsed for i = 1:10
 			d = Dict(zip([1, 2], [i, i + 1]))
 			@Test.test fp(d) == d
 		end
 		@Test.test t > 0.5
 		@Test.test t < 2.
 
-		t = @elapsed for i = 1:10
+		global t = @elapsed for i = 1:10
 			d = Dict(zip([1, 2], [i, i + 1]))
 			@Test.test fp(d) == d
 		end
 		@Test.test t < 4. # this is slow under 1.0
 
-		v = zeros(10)
+		global v = zeros(10)
 		for i = 1:2
 			@Test.test fp(v) == v
 		end
 
-		t = @elapsed for i = 1:10
-			v = i * ones(10)
+		global t = @elapsed for i = 1:10
+			global v = i * ones(10)
 			@Test.test fp(v) == v
 		end
 		@Test.test t > 0.5
 		@Test.test t < 2.
 
-		t = @elapsed for i = 1:10
-			v = i * ones(10)
+		global t = @elapsed for i = 1:10
+			global v = i * ones(10)
 			@Test.test fp(v) == v
 		end
 		@Test.test t < 4. # this is slow under 1.0
@@ -110,15 +110,15 @@ end
 	end
 
 	v3g = ReusableFunctions.maker3function(freusevector, restartdir)
-	d = [1, 2]
-	r = [1, 2]
+	global d = [1, 2]
+	global r = [1, 2]
 	for i = 1:2
 		@Test.test v3g(d) == r
 	end
 
 	r3g = ReusableFunctions.maker3function(greuse, restartdir, ["a", "b"], ["asdf", "hjkl"])
-	d = Dict("a"=>1, "b"=>3)
-	r = Dict("asdf"=>-2, "hjkl"=>3)
+	global d = Dict("a"=>1, "b"=>3)
+	global r = Dict("asdf"=>-2, "hjkl"=>3)
 	for i = 1:2
 		@Test.test r3g(d) == r
 	end
@@ -133,17 +133,17 @@ end
 		@Test.test r3g(d) == r
 	end
 
-	t = @elapsed for i = 1:10
-		d = Dict(zip(["a", "b"], [i, i + 2]))
-		r = Dict("asdf"=>-2, "hjkl"=>i * (i + 2))
+	global t = @elapsed for i = 1:10
+		global d = Dict(zip(["a", "b"], [i, i + 2]))
+		global r = Dict("asdf"=>-2, "hjkl"=>i * (i + 2))
 		@Test.test r3g(d) == r
 	end
 	@Test.test t > 0.5
 	@Test.test t < 2.
 
 	t = @elapsed for i = 1:10
-		d = Dict(zip(["a", "b"], [i, i + 2]))
-		r = Dict("asdf"=>-2, "hjkl"=>i * (i + 2))
+		global d = Dict(zip(["a", "b"], [i, i + 2]))
+		global r = Dict("asdf"=>-2, "hjkl"=>i * (i + 2))
 		@Test.test r3g(d) == r
 	end
 	@Test.test t < 2. # this is slow under 1.0
